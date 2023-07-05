c143b0a4481e55527f7fc6ceba146e534683a9de
Eric Smyth <somegoodmilk@gmail.com>
2023-07-04 20:20:19 -0400

    added authentication code for spotify

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var client_id = '0d29a13314704580a8e1dbb68e0ec812';
+var client_secret = 'd26b235197614f57ba9708b20366a313';
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
