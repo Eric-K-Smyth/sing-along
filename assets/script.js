d61c20c78dac644eb788ef07b51917b4c5c0e5e8
Eric Smyth <somegoodmilk@gmail.com>
2023-07-13 19:31:32 -0400

    made history icons change size and opacity on hover

--- assets/script.js
+++ assets/script.js
@@ -159,6 +159,7 @@ function displaySearchHistory() {
   searchHistory.forEach(function (song) {
     var albumIcon = document.createElement('img');
     albumIcon.src = song.albumIconUrl;
+    albumIcon.className = "history-icon";
     albumIcon.style.width = '100px'; // Set width to 100px
     albumIcon.style.height = '100px'; // Set height to 100px
 
